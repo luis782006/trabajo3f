39444a98457b69635d9c59603bec71bd52386fe7
Luis <luisfelipe782006@gmail.com>
2024-12-03 15:02:40 -0300

    modal

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const menuItems = document.querySelector('.menu-items');
+    // Realizamos todoas las acciones necesarias despues de asegurarnoque el DOM este listo
+        const hamburger = document.querySelector('.hamburger');
+        const menuItems = document.querySelector('.menu-items');
+        const botonAbout = document.getElementById('boton-about');
+        const modal = document.getElementById('modal');
+        const closebtn = document.querySelector('.close-btn');
 
-    hamburger.addEventListener('click', (event) => {
-        event.preventDefault();
-        menuItems.classList.toggle('active');
-    });
+        //Agregamos una clase o la quita seguen el estado en que este menuItems
+        hamburger.addEventListener('click', (event) => {
+            event.preventDefault();
+            menuItems.classList.toggle('active');
+        });
 
-    // Cerrar el menú al hacer clic en un elemento
-    document.querySelectorAll('.menu-items li').forEach(link => {
-        link.addEventListener('click', () => {
-            if (window.innerWidth <= 768) {
+        // Cerrar el menú al hacer clic en un elemento osea cada li
+        document.querySelectorAll('.menu-items li').forEach(link => {
+            link.addEventListener('click', () => {
+                // Si la ventana es menor o igual a 768px, cerrar el menú
+                if (window.innerWidth <= 768) {
+                    menuItems.classList.remove('active');
+                }
+            });
+        });
+       
+        // Manejar el cambio de tamaño de la ventana
+        window.addEventListener('resize', () => {
+            if (window.innerWidth > 768) {
                 menuItems.classList.remove('active');
             }
         });
-    });
 
-    // Cerrar el menú al hacer clic fuera
-    document.addEventListener('click', (event) => {
-        if (window.innerWidth <= 768 && 
-            !event.target.closest('.menu-items') && 
-            !event.target.closest('.hamburger')) {
-            menuItems.classList.remove('active');
-        }
-    });
+        botonAbout.addEventListener('click', () => {
+            modal.classList.remove('modal-oculto');
+            modal.classList.add('modal-visible');
+            
+        });
 
-    // Manejar el cambio de tamaño de la ventana
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 768) {
-            menuItems.classList.remove('active');
-        }
-    });
+        closebtn.addEventListener('click', () => {
+            modal.classList.remove('modal-visible');
+            modal.classList.add('modal-oculto');
+        });
 });
